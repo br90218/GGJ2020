4f5986d60bba72a728b773da1558139ec783bfdd
Isaac <izach.james@gmail.com>
2020-01-31 19:21:02 -0800

    First commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public Rigidbody2D rb;
+
+    public void Update() {
+        rb.AddForce(Vector2.right * Input.GetAxis("Horizontal"));
+    }
+}
