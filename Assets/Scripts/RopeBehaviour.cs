eb526ecd624fc00d197872d8af146d0b62008e3b
Brian Teng <askmeifyouwantmyusername@gmail.com>
2020-02-01 11:56:18 -0800

    Testing out multiple rope behaviour in TestGrounds

---
+++ Assets/Scripts/RopeBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+[RequireComponent(typeof(SpringJoint2D))]
+[RequireComponent(typeof(FixedJoint2D))]
+public class RopeBehaviour : MonoBehaviour
+{
+    public Transform OriginTransform, TargetTransform;
+
+    private LineRenderer _line;
+    private SpringJoint2D _hingeJoint;
+    private FixedJoint2D _fixedJoint;
+
+    public void Start()
+    {
+        OriginTransform = transform;
+        _line = GetComponent<LineRenderer>();
+        _line.SetPosition(0, OriginTransform.position);
+        _line.SetPosition(1, TargetTransform.position);
+        _hingeJoint = GetComponent<SpringJoint2D>();
+        _hingeJoint.connectedBody = TargetTransform.GetComponent<Rigidbody2D>();
+
+        if (transform.parent)
+        {
+            _fixedJoint.attachedRigidbody = transform.parent.GetComponent<Rigidbody2D>();
+        }
+
+
+    }
+
+
+    public void Update()
+    {
+        _line.SetPosition(0, OriginTransform.position);
+        _line.SetPosition(1, TargetTransform.position);
+
+    }
+}
