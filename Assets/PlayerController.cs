208bb1e5b9a026221192a2bb89f2b32dafc2fe70
Isaac <izach.james@gmail.com>
2020-01-31 22:15:22 -0800

    First stab at a jump

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -3,9 +3,109 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-    public Rigidbody2D rb;
+    public float MaxSpeed = 1f;
+    public float Acceleration = 1f;
+    public float Decelleration = 1f;
+    public float JumpSpeed = 1f;
+    public float JumpDeceleration = 1f;
+    public Transform Body;
+    public float JumpGraceTime = 0.02f;
+    public float Gravity;
+    public float AirSteerRatio = 0.5f;
+
+    public Transform LeftFoot;
+    public Transform RightFoot;
+
+    protected float LastGroundContactRight;
+    protected float LastGroundContactLeft;
+    protected float LastJumpTime;
+    protected Vector3 Velocity;
+    protected bool groundContact;
 
     public void Update() {
-        rb.AddForce(Vector2.right * Input.GetAxis("Horizontal"));
+        transform.position += Velocity / 2f;
+        Jump();
+        HorizontalMovement();
+        transform.position += Velocity / 2f;
+    }
+
+    protected void Jump() {
+        var leftRCH = Physics2D.Raycast(LeftFoot.position, Vector3.down, 0.15f);
+        var rightRCH = Physics2D.Raycast(RightFoot.position, Vector3.down, 0.15f);
+        var dropRCH = Physics2D.CircleCast(transform.position, 0.25f, Vector3.down, 20f);
+
+        bool leftDangle = leftRCH.collider == null;
+        bool rightDangle = rightRCH.collider == null;
+
+        if (!leftDangle) {
+            LastGroundContactLeft = Time.time;
+        }
+        if (!rightDangle) {
+            LastGroundContactRight = Time.time;
+        }
+        
+        var thresholdTime = Time.time - JumpGraceTime;
+        groundContact = (LastGroundContactLeft > thresholdTime || LastGroundContactRight > thresholdTime) && LastJumpTime < thresholdTime;
+
+        if (leftDangle && !rightDangle) {
+            Body.transform.rotation = Quaternion.Euler(0f, 0f, 20f);
+        }
+        else if (!leftDangle && rightDangle) {
+            Body.transform.rotation = Quaternion.Euler(0f, 0f, -20f);
+        }
+        else {
+            Body.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && groundContact) {
+            Velocity.y += JumpSpeed;
+            groundContact = false;
+            LastJumpTime = Time.time;
+            Debug.Log("Jump");
+        }
+
+        if (Velocity.y > 0f && !Input.GetKey(KeyCode.Space)) {
+            Velocity.y -= JumpDeceleration * Time.deltaTime;
+        }
+
+        if (leftDangle && rightDangle) {
+            Velocity.y -= Gravity * Time.deltaTime;
+        }
+        else {
+            if (Velocity.y < 0f) {
+                float maxDrop = dropRCH.distance - 0.23f;
+                if (Velocity.y < maxDrop && Mathf.Abs(Velocity.y) > Mathf.Abs(maxDrop)) {
+                    transform.position += Vector3.down * maxDrop;
+                    Velocity.y = 0f;
+                }
+            }
+        }
+    }
+
+    protected void HorizontalMovement() {
+        float rightAxis = Input.GetKey(KeyCode.RightArrow) ? 1f : 0f;
+        rightAxis = Input.GetKey(KeyCode.LeftArrow) ? -1f : rightAxis;
+        float acceleration = Acceleration;
+        bool accelerating = true;
+        float xDir = Velocity.x == 0f ? 0f : Velocity.x / Mathf.Abs(Velocity.x);
+        if (xDir != rightAxis && xDir != 0f) {
+            Debug.Log("Stop");
+            acceleration = Decelleration;
+            accelerating = false;
+            if (Velocity.x != 0f) {
+                rightAxis = -xDir;
+            }
+        }
+        if (!groundContact) {
+            acceleration *= AirSteerRatio;
+        }
+        var change = Vector3.right * rightAxis * Time.deltaTime * acceleration;
+        Velocity += change;
+        if (!accelerating && Velocity.sqrMagnitude < change.sqrMagnitude) {
+            Velocity = Vector3.zero;
+        }
+        if (Velocity.sqrMagnitude > MaxSpeed * MaxSpeed) {
+            Velocity = Velocity.normalized * MaxSpeed;
+        }
     }
 }
