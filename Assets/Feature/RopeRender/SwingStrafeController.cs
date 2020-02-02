0e019bb6b603ba7b0bda34f1c732b7862bfdd41f
Isaac <izach.james@gmail.com>
2020-02-01 21:42:59 -0800

    Fix the transition into and out of swing state

--- Assets/Feature/RopeRender/SwingStrafeController.cs
+++ Assets/Feature/RopeRender/SwingStrafeController.cs
@@ -28,8 +28,7 @@ public class SwingStrafeController : MonoBehaviour
     private Vector3 handOffset;
     private Transform playerTransform;
     private const float g = 9.7f;
-
-
+    private Vector3 lastFrameVelocity;
 
     public void StartSwing(Transform hingePoint)
     {
@@ -38,32 +37,20 @@ public class SwingStrafeController : MonoBehaviour
         var dis =  hand.position - hingePoint.position;
         ropeLength = dis.magnitude;
         
-        angularSpeed = playerController.Velocity.magnitude / ropeLength;
-        angularSpeed *= speedToAngular;
-        if (playerController.Velocity.x < 0)
-        {
-            angularSpeed *= -1;
-        }
+        angularSpeed = Vector3.Dot(Vector3.Cross(dis, Vector3.back).normalized, playerController.Velocity) * Mathf.Rad2Deg / ropeLength;
         if (dis.x > 0)
         {
-            //angularSpeed *= -1;
             angle = Mathf.Asin(dis.y/ropeLength);
         }
         else
         {
             angle = 180*Mathf.Deg2Rad- Mathf.Asin(dis.y/ropeLength);
         }
-        //Temp
-        //angularSpeed = -1;
-
-        //dir.y *= -1;
         
-
         energyTotal = 0.5f * ropeLength * ropeLength * angularSpeed * angularSpeed;
         rotationEnergy = energyTotal;
         gravitationalPotentialEnergy = (ropeLength - (hingePoint.position.y - hand.position.y)) * g;
         energyTotal += gravitationalPotentialEnergy;
-        
     }
     
     private void Awake()
@@ -73,18 +60,15 @@ public class SwingStrafeController : MonoBehaviour
     }
     private void LateUpdate()
     {
+        UpdateRopePhysic();
         if (Input.GetKeyDown(KeyCode.Space))
         {
             enabled = false;
             grappleHookController.enabled = true;
             playerController.enabled = true;
-            playerController.Velocity = new Vector3(-angularSpeed * ropeLength * Mathf.Sin(angle),angularSpeed * ropeLength * Mathf.Cos(angle),0) * angularToSpeed;
-            //Debug.Log(playerController.Velocity);
-            //playerController.Velocity = new Vector3();
+            playerController.Velocity = lastFrameVelocity / Time.deltaTime;
             ropeRenderer.ClearRope();
-            return;
         }
-        UpdateRopePhysic();
     }
 
     private void UpdateRopePhysic()
@@ -92,41 +76,24 @@ public class SwingStrafeController : MonoBehaviour
         float rightAxis = Input.GetKey(KeyCode.D) ? 1f : 0f;
         rightAxis = Input.GetKey(KeyCode.A) ? -1f : rightAxis;
         angularSpeed *= (1-(Time.deltaTime * 0.05f));
-//        if (angularSpeed > 0)
-//        {
-//            angularSpeed -= Time.deltaTime * angularDrag;
-//            if (angularSpeed < 0)
-//            {
-//                angularSpeed = 0;
-//            }
-//        }
-//        else
-//        {
-//            angularSpeed += Time.deltaTime * angularDrag;
-//            if (angularSpeed > 0)
-//            {
-//                angularSpeed = 0;
-//            }
-//        }
         angularSpeed += rightAxis * swingAcceleration * Time.deltaTime;
-        
         
         var rotationEnergyNew = 0.5f * ropeLength * ropeLength * angularSpeed * angularSpeed;
         energyTotal += (rotationEnergyNew - rotationEnergy);
         rotationEnergy = rotationEnergyNew;
         
         angle += angularSpeed * Time.deltaTime;
-        hand.position = hingePoint.position + new Vector3(Mathf.Cos(angle) * ropeLength, Mathf.Sin(angle) * ropeLength, 0);
+        var handPosition =  hingePoint.position + new Vector3(Mathf.Cos(angle) * ropeLength, Mathf.Sin(angle) * ropeLength, 0);
+        Vector3 lastPosition = playerTransform.position;
+        playerTransform.position = handPosition - handOffset;
+        lastFrameVelocity = playerTransform.position - lastPosition;
         UpdateRope(hand.position);
-        playerTransform.position = hand.position - handOffset;
         var newGPE = (ropeLength - (hingePoint.position.y - hand.position.y)) * g;
         gravitationalPotentialEnergy = newGPE;
         rotationEnergy = energyTotal - newGPE;
-        //Debug.Log("Energy"+rotationEnergy);
-        //Debug.Log(angularSpeed);
         if (rotationEnergy > 0)
         {
-            if (angularSpeed>0)
+            if (angularSpeed > 0)
             {
                 angularSpeed = Mathf.Sqrt((rotationEnergy * 2)/(ropeLength*ropeLength));
             }
